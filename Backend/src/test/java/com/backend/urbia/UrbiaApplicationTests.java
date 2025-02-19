a7aec1132175d72f3756dcf5617e0281dd216bb8
Albert0GR <albertogr0121@gmail.com>
2025-02-19 16:36:44 -0600

    🛠️ Chore: Creación de proyecto con java Spring Boot

---
+++ Backend/src/test/java/com/backend/urbia/UrbiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.urbia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrbiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
