46fd3132397eca7ef48cd5c187dcc8f5441068ea
Ronny <rjba08@gmail.com>
2025-02-23 13:28:07 -0600

    prueba backend final

--- Backend/src/main/java/com/backend/urbia/UrbiaApplication.java
+++ Backend/src/main/java/com/backend/urbia/UrbiaApplication.java
@@ -3,12 +3,12 @@ package com.backend.urbia;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-
 @SpringBootApplication
 public class UrbiaApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(UrbiaApplication.class, args);
 	}
+
 }
