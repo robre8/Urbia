0288969ed08ba829505c997ddcbbf8a5ca600e12
Albert0GR <albertogr0121@gmail.com>
2025-02-24 22:45:00 -0600

    🛠️ Configuracion de pom.xml

--- Backend/src/main/java/com/nocountry/urbia/controller/ControllerHolaMundo.java
+++ Backend/src/main/java/com/nocountry/urbia/controller/ControllerHolaMundo.java
@@ -1,4 +1,4 @@
-package com.nocountry.urbia.Controller;
+package com.nocountry.urbia.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
