8ec20cec06c74aade89eb212671b4357ede0c591
Ronny <rjba08@gmail.com>
2025-02-27 18:11:21 -0600

    Modificacion incluyendo controlador y configuracion de Websockets, ademas se agrego que la respuesta de la autenticacion de el nombre a parte del correo

--- Backend/src/main/java/com/nocountry/urbia/dto/response/JwtResponse.java
+++ Backend/src/main/java/com/nocountry/urbia/dto/response/JwtResponse.java
@@ -2,13 +2,11 @@ package com.nocountry.urbia.dto.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
-@NoArgsConstructor
 
 public class JwtResponse {
 
@@ -19,5 +17,47 @@ public class JwtResponse {
     private String nombre;
 
 
+    public JwtResponse() {}
 
+    public JwtResponse(String token, Long id, String email, String nombre) {
+        this.token = token;
+        this.id = id;
+        this.email = email;
+        this.nombre = nombre;
+
+    }
+
+
+    // Getters y Setters
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
