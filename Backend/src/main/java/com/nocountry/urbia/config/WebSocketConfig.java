b907216bf675e97345389b4ff92eeeaf72d5d9da
Ronny <rjba08@gmail.com>
2025-02-27 18:13:04 -0600

    Modificacion incluyendo controlador y configuracion de Websockets, ademas se agrego que la respuesta de la autenticacion de el nombre a parte del correo

--- Backend/src/main/java/com/nocountry/urbia/config/WebSocketConfig.java
+++ Backend/src/main/java/com/nocountry/urbia/config/WebSocketConfig.java
@@ -20,7 +20,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/app"); //prefijo de envio de datos del cliente
-        registry.enableSimpleBroker("/topic"); //enviados a topic o queue
+        registry.enableSimpleBroker("/topic"); //enviados a topic
     }
 }
 
