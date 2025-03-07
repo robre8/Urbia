5cdebf7e76fe28483c3ffe6b117d35c3a1dde262
AlejandroLunaDev <alejandrolunadev@gmail.com>
2025-03-06 22:13:06 -0300

    Replace NewReportForm with CleanReportForm in AppRoutes component

--- Frontend/src/routes/AppRoutes.jsx
+++ Frontend/src/routes/AppRoutes.jsx
@@ -2,13 +2,15 @@ import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import HomePage from '@/features/home/pages/HomePage';
 import Menu from '@/components/layout/Menu';
 import NewReportForm from '@/features/NewReportForm/NewReportForm';
+import CleanReportForm from '@/features/reports/form/CleanReportForm';
 
 export default function AppRoutes() {
   return (
     <BrowserRouter>
     <div className='absolute z-[9999]'>
       <Menu />
-      <NewReportForm />
+      {/* <NewReportForm /> */}
+      <CleanReportForm />
 
     </div>
       <Routes>
