bf2f3f8f769efd86094adcd989e488c44d3454b2
AlejandroLunaDev <alejandrolunadev@gmail.com>
2025-03-07 23:35:17 -0300

    Cambio de back

--- Frontend/src/lib/api/axios.js
+++ Frontend/src/lib/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://s21-19-t-webapp-production.up.railway.app/",
+  baseURL: "https://handsome-wisdom-production.up.railway.app/",
   timeout: 15000,
   headers: {
     "Content-Type": "application/json",
