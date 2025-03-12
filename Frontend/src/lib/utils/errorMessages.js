5c64165b4dd2e4204a820997cb7de5263309fff3
AlejandroLunaDev <alejandrolunadev@gmail.com>
2025-03-11 23:44:31 -0300

    Update error messages for geolocation and improve ErrorMessage component styling

--- Frontend/src/lib/utils/errorMessages.js
+++ Frontend/src/lib/utils/errorMessages.js
@@ -1,17 +1,17 @@
 export function getGeolocationErrorMessage(geolocationStatus, error) {
     if (geolocationStatus === "browser_denied") {
-      return "Permiso de ubicación denegado. Habilita la ubicación en tu navegador o elige una ciudad.";
+      return "Permiso de ubicación denegado. Por favor, habilita la ubicación en tu navegador o elige una ciudad.";
     } else if (
       geolocationStatus === "browser_unavailable" ||
       geolocationStatus === "browser_timeout" ||
       geolocationStatus === "ip_error_default" ||
       geolocationStatus === "timeout_default"
     ) {
-      return "Ay! No pudimos encontrar tu ubicación. Habilítala en el navegador o elige una ciudad para continuar."; //este cambiar
+      return "Ay! No pudimos encontrar tu ubicación. Habilítala en el navegador o elige una ciudad para continuar.";
     } else if (geolocationStatus === "unsupported") {
       return "Tu navegador no soporta geolocalización. Usando ubicación por defecto o elige una ciudad.";
     } else if (error) {
-      return `Error al obtener ubicación: ${error}. Puedes elegir una ciudad.`;
+      return "Ay! No pudimos encontrar tu ubicación. Habilítala en el navegador o elige una ciudad para continuar.";
     }
     return null;
   }
